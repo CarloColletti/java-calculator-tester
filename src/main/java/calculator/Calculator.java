bbdbd7a99b6e900fe0a101eb58586b906b7e03df
CarloColletti~ <carlocolletti5@gmail.com>
2023-06-19 15:37:25 +0200

    Add Calculator class

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package calculator;
+public class Calculator {
+    //addizione
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    //sottrazione
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    //divisione
+    public float divide(float num1, float num2) {
+        if(num2 == 0) {
+            throw new IllegalArgumentException("Il secondo argomento non può essere un numero negativo");
+        }
+        return num1 / num2;
+    }
+
+    //moltiplicazione
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+}
